e4fe28b0b6362e1176d0d9805473c0c3239b3212
James Burke <james.burke1203@gmail.com>
2016-06-25 10:59:00 +0200

    updated to web2py v2.14.6

--- controllers/default.py
+++ controllers/default.py
@@ -1,33 +1,40 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
-#########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-#########################################################################
+# -------------------------------------------------------------------------
+# This is a sample controller
+# - index is the default action of any application
+# - user is required for authentication and authorization
+# - download is for downloading files uploaded in the db (does streaming)
+# -------------------------------------------------------------------------
+
 
 def index():
     """ Display the index page """
     index = db(db.cms_page.page_index==0).select(db.cms_page.ALL).first().as_dict()
 
-    return dict(index=index)
+    return locals()
 
 def page():
     """" Show a page """
+    if not request.args(0):
+        redirect(URL('index'))
 
     try:
         page = db(db.cms_page.id == int(request.args(0))).select().first().as_dict()
-    except:
-        page = db(db.cms_page.title=='%s' % request.args(0).title()).select().first().as_dict() or redirect(URL('default', 'index'))
+    except ValueError:
+        try:
+            page = db(db.cms_page.title=='%s' % request.args(0).title()).select().first().as_dict() or redirect(URL('default', 'index'))
+        except:
+            raise HTTP(404)
 
     if page['members_only'] and not auth.user:
         redirect(URL('index'))
     elif page['members_only'] and not auth.has_membership('member', auth.user.id):
         redirect(URL('index'))
     else:
-        return dict(page=page)
+        return locals()
+
 
 def user():
     """
@@ -43,6 +50,7 @@ def user():
         @auth.requires_membership('group name')
         @auth.requires_permission('read','table name',record_id)
     to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
     """
     return dict(form=auth())
 
@@ -83,14 +91,15 @@ def call():
     return service()
 
 def sitemap():
-    sitemap=TAG.urlset(_xmlns="http://www.sitemaps.org/schemas/sitemap/0.9") # & (db.page.published==True)
-    sitemap.append(TAG.url(TAG.loc(request.env.http_host)))
+    sitemap=TAG.urlset(_xmlns="http://www.sitemaps.org/schemas/sitemap/0.9") # & (db.cms_page.published==True)
+    sitemap.append(TAG.url(TAG.loc('http://%s' % request.env.http_host),TAG.priority(1.0)))
 
-    posts = db((db.page.page_index >= 0) & (db.page.main_menu == True) & (db.page.published == True) & (db.page.members_only == False)).select(db.page.ALL, orderby=[db.page.page_index])
+    posts = db((db.cms_page.page_index >= 0) & (db.cms_page.main_menu == True) & (db.cms_page.published == True) & (db.cms_page.members_only == False)).select(db.cms_page.ALL, orderby=[db.cms_page.page_index])
 
     for item in posts:
-        if item.published:
-            sitemap.append(TAG.url(TAG.loc('%s/page/%s' %(request.env.http_host, item.title))))
+        if item.published and item.page_index != 0:
+            sitemap.append(TAG.url(TAG.loc('http://%s/page/%s/%s' %(request.env.http_host, item.id, item.title))))
 
-    sitemap.append(TAG.url(TAG.loc('%s/sitemap.xml' % request.env.http_host)))
+    sitemap.append(TAG.url(TAG.loc('http://%s/sitemap.xml' % request.env.http_host)))
     return '<?xml version="1.0" encoding="UTF-8"?>\n%s' %sitemap.xml()
+
